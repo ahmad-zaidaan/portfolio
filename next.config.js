8be6338dc530383f7e2ce7bc946a1cc0d45488ca
Ahmad Zaidaan <5053251021@student.its.ac.id>
2026-09-11 14:18:55 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+// const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+// module.exports = withNextra()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
+export default nextConfig
